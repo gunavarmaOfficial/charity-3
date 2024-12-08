ac411039f78f0d0ecedef82a4d9ec4f9e2108432
gunavarmaOfficial <gunavarmaofficial00@gmail.com>
2024-12-08 13:46:55 +0530

    removed middleware

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware() {
-  const response = NextResponse.next();
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  response.headers.set("Access-Control-Allow-Headers", "Content-Type");
-  return response;
-}
