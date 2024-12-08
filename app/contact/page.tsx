d7181bf91e7e33d923929e6d076666d30eb10e99
gunavarmaOfficial <gunavarmaofficial00@gmail.com>
2024-12-08 17:41:06 +0530

    asdasdasd

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -24,7 +24,7 @@ export default function ContactPage() {
       const response = await fetch("api/contact", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
+          // "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
